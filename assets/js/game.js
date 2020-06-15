0ab38008e6d809f94343969b62f8cb4427880772
savan patel <savanpatel2003@gmail.com>
2020-06-14 23:23:01 -0400

    created gamejs and index

---
+++ assets/js/game.js
@@ -0,0 +1,14 @@
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+console.log("This logs a string, good for leabing yourself a message");
+// this will do match and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
